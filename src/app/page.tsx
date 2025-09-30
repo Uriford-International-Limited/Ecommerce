d4e6646f8ba47142ce3f146cd7b8c6109bb188d5
Tajrian Afroz <tajrianafroz1343@gmail.com>
2025-09-30 11:56:32 +0600

    Account Login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="min-h-screen flex justify-center items-center">
-      <h1 className="text-7xl font-bold">Shopshere</h1>
-    </div>
-  );
+  return <div className="min-h-screen flex justify-center items-center"></div>;
 }
