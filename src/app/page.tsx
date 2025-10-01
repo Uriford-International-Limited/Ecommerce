a6a6358c965f3a8b0cc03526dc6a36b9a848a911
Al amin <alaminmridha2004@gmail.com>
2025-10-01 22:23:10 +0600

    Add Limited Time promotional sections and user SVG icon
    - Created a new SVG file for user icon at public/user2.svg.
    - Implemented LimitedTime component with three promotional cards in src/app/(pages)/home/sections/LimitedTime.tsx.
    - Implemented LimitedTime2 component with two additional promotional cards in src/app/(pages)/home/sections/LimitedTime2.tsx.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
-import CardWrapper from "@/components/ui/CardWrapper";
 import Hero from "./(pages)/home/sections/Hero";
+import CardWrapper from "@/components/ui/CardWrapper";
 import ProductCard from "@/components/ui/ProductCard";
 import { arrivalDatas, arrivalDatas2 } from "@/datas";
+import LimitedTime from "./(pages)/home/sections/LimitedTime";
+import LimitedTime2 from "./(pages)/home/sections/LimitedTime2";
+import { Star } from "lucide-react";
+import Image from "next/image";
 
 export default function Page() {
   return (
-    <>
+    <div className="space-y-10">
       <Hero />
+
+      <LimitedTime />
 
       <CardWrapper
         label="New Arrivals"
@@ -17,6 +23,8 @@ export default function Page() {
         ))}
       </CardWrapper>
 
+      <LimitedTime2 />
+
       <CardWrapper
         label="Featured Products"
         description="Do not miss the current offers until the end of March."
@@ -26,6 +34,44 @@ export default function Page() {
           <ProductCard direction="horizontal" key={item._id} item={item} />
         ))}
       </CardWrapper>
-    </>
+
+      <CardWrapper
+        label="Popular Companies"
+        description="Some of the new products arriving this weeks"
+        className="flex"
+      >
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <div key={idx} className="min-w-[300px] grow p-4 border">
+            <div className="flex gap-3 border-b pb-4">
+              <Image
+                src="/user2.svg"
+                width={300}
+                height={300}
+                alt="User Image"
+                className="size-[81px] rounded"
+              />
+
+              <div>
+                <h2 className=" font-bold">Machic</h2>
+                <p className="text-xs my-1">Featured</p>
+                <div className="flex items-center gap-0.5">
+                  {Array.from({ length: 5 }).map((_, idx) => (
+                    <Star
+                      key={idx}
+                      size={13}
+                      className={`${idx <= 3 && "fill-amber-400"}`}
+                    />
+                  ))}
+                  <span className="ms-2">40</span>
+                </div>
+              </div>
+            </div>
+            <p className="text-sm">
+              Good quality product can only be found in good stores
+            </p>
+          </div>
+        ))}
+      </CardWrapper>
+    </div>
   );
 }
